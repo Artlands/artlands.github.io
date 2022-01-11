7fcc732ffcc8ad4c109b498b3cc69885a47439a3
artlands <lijie0416@gmail.com>
2022-01-11 14:17:55 -0500

    update

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,17 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Washington, D.C.",
+    38.9074322,
+    -77.0350922
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Lubbock, Texas",
+    33.5635206,
+    -101.879336
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Saint Louis, Missouri",
+    38.6264256,
+    -90.1995853
   ]
 ];
